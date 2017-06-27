61293f18eba5c832a6170db477a891914f65dc55
Hejx <531601727@qq.com>
2017-06-27 15:09:12 +0800

    save

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { AppContainer } from 'react-hot-loader'
+
+import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <AppContainer>
+        <App/>
+    </AppContainer>,
+    document.getElementById('root')
+);
 registerServiceWorker();
+
+if (module.hot) {
+  module.hot.accept('./containers/App', () => {
+    const NextApp = require('./containers/App').default;
+    ReactDOM.render(
+      <AppContainer>
+        <NextApp/>
+      </AppContainer>,
+      document.getElementById('root')
+    );
+  });
+}
